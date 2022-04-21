41f4b419047e64e87030de378b92f3fed484eec6
Kushaj Arora <arorakushaj@gmail.com>
2022-04-21 14:19:38 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let userBox = document.querySelector('.header .header-2 .user-box');
+
+document.querySelector('#user-btn').onclick = () =>{
+   userBox.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .header-2 .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   userBox.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   userBox.classList.remove('active');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .header-2').classList.add('active');
+   }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    alert("Succesfully submitted");
+  });
+});
